86f157b62077322ec104e83edc97c920ab311a56
Dom De Re <domdere@domdere.com>
2013-09-17 22:28:39 +1000

    Made a miniscule amount of progress

--- src/Text/Parser/PolyRestSpect.hs
+++ src/Text/Parser/PolyRestSpect.hs
@@ -2,13 +2,16 @@ module Text.Parser.PolyRestSpect
     (   restFile
     ) where
 
-import Control.Applicative
 import Control.Monad
 import Data.Text
-import Text.ParserCombinators.Poly.Text hiding ( many, optional, (<|>) )
+import Text.Parse
 
 import Data.RestSpec
 import Text.Parser.RestSpect.Expr
 
 restFile :: Parser RestSpec
 restFile = error "TODO"
+
+-- | A Parser For a DataNameExpr
+dataNameExpr :: Parser DataNameExpr
+dataNameExpr = DataNameExpr `fmap` many alphanum
